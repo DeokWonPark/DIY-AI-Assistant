cb3803b09458c4437fc0055ee02fa73de899d33b
20150351 <sky6min@gmail.com>
2019-10-06 16:37:06 +0900

    chatlist 변경본

--- ChatList.vue
+++ ChatList.vue
@@ -5,6 +5,9 @@
       <li class="list-group-item" v-for="message in messages" :key="message.id" >
         <span class="float-left">{{message}}</span>        
       </li>
+      <li class="list-group-item">
+        <span class="float-left"> {{typing}} </span>
+      </li>
       <!-- <li class="list-group-item" v-for="user in users" :key="user.id">
         <span class="float-left" id="join">{{user}}님이 입장하셨습니다</span>
       </li> -->
@@ -15,9 +18,22 @@
 <script>
 
 export default {
+  data: function() {
+    return {
+      typing: ''
+    };
+  },
   name: "HelloWorld",  
   props: ["messages","users"],
+  created() {
+    this.$socket.on("typing", data => {
+    // when "chat-message" comes from the server
+    console.log("msg received from server");
+    this.typing = data.typing
+  });
+  }
 };
+  
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
